acbd5b09f009555a1a02d112ad31e02492379a9d
CocoaFennec <cocoafennec@outlook.com>
2019-05-29 20:51:42 +0100

    Added zoom in to the program
    -added zooming using the mouse wheel
    -added calculatnig distance between bodies

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from classes.vector import vector2
 from classes.camera import camera
 
 import pygame as pg
+import math
 
 #initialize variables
 bodies = [body(100,100, 50), body(200,200,50)]
+G = 0.0000000000667408
 
 #initialize pg
 pg.init()
@@ -28,7 +30,7 @@ done = False
 
 while not done:
     for event in pg.event.get():
-        #if end the game if the user closes it
+        #end the game if the user closes it
         if event.type == pg.QUIT:
             done = True
 
@@ -37,12 +39,18 @@ while not done:
             #lmb
             if event.button == 1:
                 pos = pg.mouse.get_pos()
-                bodies.append(body(pos[0], pos[1]))
+                bodies.append(body(int((pos[0] + camera.position.x)/camera.zoom), int((pos[1] + camera.position.y)/camera.zoom)))
 
             #mmb
             if event.button == 2:
                 move_camera = True
                 start_pos = pg.mouse.get_pos()
+
+            if event.button == 4:
+                camera.zoom *= 1.1
+
+            if event.button == 5:
+                camera.zoom *= 0.9
 
         if event.type == pg.MOUSEBUTTONUP:
             #rmb
@@ -60,10 +68,25 @@ while not done:
 
             start_pos = pg.mouse.get_pos()
 
+    for subject in bodies:
+
+        attraction = vector2(0,0)
+
+        for attractor in bodies:
+
+            if attractor != subject:
+                distance = int(math.sqrt((attractor.position.x - subject.position.x)**2 + (attractor.position.y - subject.position.y)**2))
+                attraction += (attractor.mass/((distance)**3)) * (attractor.position + (-1 * subject.position))
+
+
+
 
     screen.fill((0,0,0))
 
     for active_body in bodies:
-        pg.draw.circle(screen, (0,0,255), (active_body.position.x - camera.position.x, active_body.position.y-camera.position.y), active_body.radius)
+        pg.draw.circle(screen, (0,0,255),
+         (int(active_body.position.x*camera.zoom - camera.position.x),
+          int(active_body.position.y*camera.zoom - camera.position.y)),
+           int(active_body.radius*camera.zoom))
         
     pg.display.flip()
