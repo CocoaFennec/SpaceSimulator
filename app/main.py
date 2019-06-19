638ee8992fab4466f805d88edd0e57267d1b6372
danny_riley <danny_riley@outlook.com>
2019-06-19 17:07:23 +0100

    Fixed zoom
    Fixed problem with math being imath and zoom.
    
    -Broken planet spawning

--- app/main.py
+++ app/main.py
@@ -6,26 +6,28 @@ import pygame as pg
 import math
 
 #initialize variables
-bodies = [body(100,100, 50, 100)]
+bodies = [body(0, 0, 50, 100)]
 G = 0.0000000000667408
 
 #initialize pg
 pg.init()
 clock = pg.time.Clock()
 dt = 0
+screen_height = 900
+screen_width = 900
 
 #gets screen resolution
-info_object = pg.display.Info()
+#info_object = pg.display.Info()
 
 #sets up camera
 camera = camera(0,0)
-camera_offset = [info_object.current_w/2, info_object.current_h/2]
-camera.offset = vector2(camera_offset[0], camera_offset[1])
+camera_offset = [screen_width/2, screen_height/2]
+camera.offset = vector2(0, 0)
 move_camera = False
 
 #sets up the screen to display the game
 #screen = pg.display.set_mode((info_object.current_w, info_object.current_h), pg.FULLSCREEN)
-screen = pg.display.set_mode((1920, 1080), pg.NOFRAME)
+screen = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption("Space Simulation")
 
 done = False
@@ -41,7 +43,7 @@ while not done:
             #lmb
             if event.button == 1:
                 pos = pg.mouse.get_pos()
-                bodies.append(body(int((pos[0] + camera.position.x)/camera.zoom), int((pos[1] + camera.position.y)/camera.zoom)))
+                bodies.append(body(int((pos[0] + camera.position.x + camera_offset[0])/camera.zoom), int((pos[1] + camera.position.y + camera_offset[1])/camera.zoom)))
 
             #mmb
             if event.button == 2:
@@ -65,8 +67,8 @@ while not done:
 
             moved = [end_pos[0]-start_pos[0], end_pos[1]-start_pos[1]]
 
-            camera.position.x -= moved[0]
-            camera.position.y -= moved[1]
+            camera.position.x -= moved[0]/camera.zoom
+            camera.position.y -= moved[1]/camera.zoom
 
             start_pos = pg.mouse.get_pos()
 
@@ -77,7 +79,7 @@ while not done:
         for attractor in bodies:
 
             if attractor != subject:
-                distance = imath.sqrt((attractor.position.x - subject.position.x)**2 + (attractor.position.y - subject.position.y)**2)
+                distance = math.sqrt((attractor.position.x - subject.position.x)**2 + (attractor.position.y - subject.position.y)**2)
                 if distance != 0:
                     attraction += (attractor.position - subject.position) * (attractor.mass/((distance)**3))
 
@@ -90,8 +92,8 @@ while not done:
 
     for active_body in bodies:
         pg.draw.circle(screen, (0,0,255),
-         (int(active_body.position.x*camera.zoom - camera.position.x*camera.zoom),
-          int(active_body.position.y*camera.zoom - camera.position.y*camera.zoom)),
+         (int(active_body.position.x*camera.zoom - camera.position.x*camera.zoom + camera_offset[0]),
+          int(active_body.position.y*camera.zoom - camera.position.y*camera.zoom + camera_offset[1])),
            int(active_body.radius*camera.zoom))
 
     pg.display.flip()
